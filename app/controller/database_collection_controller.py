ee7130045203c29fb9babe01e6bc37bec131b1db
lowercase-tousif <toisiftasrik@gmail.com>
2025-05-19 01:52:29 +0600

    Created a separete file for accessing specific collection

---
+++ app/controller/database_collection_controller.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+import os
+
+# Users collection 
+def getUsersCollection():
+    cluster = MongoClient(os.getenv("MONGO_URI"))
+    main_database = cluster['Sketchify']
+    users_collection = main_database['Users']
+    return users_collection
+
+# Categories Collection
+def getCategoriesCollection():
+    cluster = MongoClient(os.getenv("MONGO_URI"))
+    main_database = cluster['Sketchify']
+    categories_collection = main_database['Categories']
+    return categories_collection
